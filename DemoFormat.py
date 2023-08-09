d4599517399b1cb3a84019f8f35ab0600b05b757
papasmf1 <papasmf1@gmail.com>
2023-08-09 13:52:55 +0900

    파일 연습 완료

--- DemoFormat.py
+++ DemoFormat.py
@@ -17,6 +17,10 @@ print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
 print("{0:,}".format(15000000))
 
+for i in range(0,10):
+    #동적으로 주소를 생성 
+    strURL = "https://www.ycampus.co.kr/?page=" + str(i)
+    print(strURL)
 
 
 
